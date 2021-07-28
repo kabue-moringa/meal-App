513f62f88279219b5d6723fd4a945f628717ce8b
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-07-28 23:09:11 +0300

    make changes

--- app/src/main/java/com/moringaschool/mealapp/ui/LoginActivity.java
+++ app/src/main/java/com/moringaschool/mealapp/ui/LoginActivity.java
@@ -2,15 +2,35 @@ package com.moringaschool.mealapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.moringaschool.mealapp.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
 
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @BindView(R.id.registerTextView)
+    TextView mRegisterTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (view == mRegisterTextView) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+
     }
 }
